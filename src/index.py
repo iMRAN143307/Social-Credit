966a2cb4a4e0f789aa5103fc17b79fdef963ebe6
Bolb2019 <zachnoahstein@gmail.com>
2026-08-08 00:48:02 -0400

    feat: added a balance you can check and add/subtract from, still needs to connect to database but this is a good start.

--- src/index.py
+++ src/index.py
@@ -4,16 +4,30 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-@app.event("app_mention")
-def handle_mention(event, say, logger):
-    print(f"App mentioned by user: {event['user']}")
-    logger.info(f"App mentioned by user: {event['user']}")
-    say(f"Hi <@{event['user']}>!")
+balance = 100
+#---------CODE---------
+
+@app.command("/balance-check")
+def repeat_text(ack, respond):
+    ack()
+    respond(f"Your balance is ${balance}.")
+
+@app.command("/balance-add")
+def repeat_text(ack, respond, command):
+    ack()
+    balance += int(command['text'])
+    respond(f"your new balance is ${balance}")
+
+@app.command("/balance-subtract")
+def repeat_text(ack, respond, command):
+    ack()
+    balance -= int(command['text'])
+    respond(f"your new balance is ${balance}")
+
+#---------BOTTOM THING---------
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
     handler.start()
-    print("Running!")
