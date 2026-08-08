096d46aed0734b8fdde8bcfe9a7386d9f6a56c3c
Bolb2019 <zachnoahstein@gmail.com>
2026-08-08 11:20:00 -0400

    ok sure  ewfjhbegkrjrthrvrmr v

--- src/index.py
+++ src/index.py
@@ -22,35 +22,9 @@ balance = 100
 
 #For checking balance
 @app.command("/social-credit")
-def repeat_text(ack, respond, body):
+def repeat_text(ack, respond):
     ack()
-    # user = body.get("user_id")
-
-    # user_found = False
-    # try:
-    #     with open('users.csv', newline='') as csvfile:
-    #         reader = csv.reader(csvfile)
-    #         for row in reader:
-    #             if row and row[0] == user:
-    #                 user_found = True
-    #                 break
-    # except FileNotFoundError:
-    #     user_found = False
-        
-    # if not user_found:
-    #     respond("you aren't signed up! run /social-credit-signup to sign up")
-    #     return
-
-    respond(f"Your social credit is ☭{balance} / ☭100")
-
-# @app.command("/social-credit-signup")
-# def repeat_text(ack, respond, body):
-#     ack()
-#     user = body.get("user_id")
-#     with open('users.csv', 'a', newline='') as file:
-#         writer = csv.writer(file)
-#         writer.writerow([user])
-#     respond(f"Signed up :3 you can undo this by typing /social-credit-signout")
+    respond(f"Your balance is ☭{balance}.")
 
 #for adding to balance
 @app.command("/social-add")
@@ -69,6 +43,7 @@ def repeat_text(ack, respond, command):
 #For mass survellance
 @app.event("message")
 def handle_message_events(event, client):
+    print("heard")
     if event.get("bot_id") or event.get("subtype"):
         return
 
@@ -83,29 +58,13 @@ def handle_message_events(event, client):
     previous_display_name = get_display_name(client, previous_user) if previous_user else None
 
     print(previous_user)
-
-    # user_found = False
-    # try:
-    #     with open('users.csv', newline='') as csvfile:
-    #         reader = csv.reader(csvfile)
-    #         for row in reader:
-    #             if row and row[0] == user:
-    #                 user_found = True
-    #                 break
-    # except FileNotFoundError:
-    #     user_found = False
-    
-    # if not user_found:
-    #     print(f"User {user} not found in users.csv.")
-    #     return
-
     if previous_user != user and previous_user != None:
         response_text = (f"User: {previous_user}, Display name: {previous_display_name}, Response: {message_text}")
         print(response_text)
 
         with open('data.csv', 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow([response_text])
+                writer = csv.writer(file)
+                writer.writerow([response_text])
 
 #---------BOTTOM THING---------
 
